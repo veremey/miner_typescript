13310a0d6d37eb5220d9fb1e2e10fd9b59a55408
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-06-02 16:15:00 +0200

    count-display refactor

--- src/ui/count-display.tsx
+++ src/ui/count-display.tsx
@@ -2,11 +2,16 @@ import * as React from "react";
 import cx from "clsx";
 import { scope } from "../lib/utils";
 
-function CountDisplay({ count, className }) {
+interface CountDisplayProps {
+	count: number;
+	className?: string;
+}
+
+const CountDisplay = ({ count, className }: CountDisplayProps) => {
 	let countString = String(Math.max(Math.min(count, 999), -99));
 	return (
 		<div className={cx(scope("count-display"), className)}>{countString}</div>
 	);
-}
+};
 
 export { CountDisplay };
